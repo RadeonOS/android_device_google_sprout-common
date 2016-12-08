f57068ec9ddb63c9469f558cc27e0b473a626a06
ujwalp15 <ujwalp09@gmail.com>
2016-12-08 11:05:05 +0530

    sprout-common: add GraphicBuffer shim for camera.mt6582.so

--- libshim/Android.mk
+++ libshim/Android.mk
@@ -8,3 +8,13 @@ LOCAL_MODULE := libshim_mtkril
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := ui/GraphicBuffer.cpp
+
+LOCAL_C_INCLUDES := gui
+LOCAL_SHARED_LIBRARIES := libgui libutils liblog libbinder libandroid libui
+LOCAL_MODULE := libshim_camera
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)
